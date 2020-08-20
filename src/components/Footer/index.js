2c2bbc432174833ad32513ece3147df157db241a
Athena Fernandes Sarantôpoulos <athenasaran@gmail.com>
2020-08-20 16:31:46 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,28 @@ import "./styles.css";
 function Footer() {
   return (
     <footer>
-      <p>Orgulhosamente criado por Athena, João, Jeniffer e Leonardo </p>
+      <p>
+        Orgulhosamente criado por
+        <a href="https://github.com/athenasaran" target="_blank">
+          {" "}
+          Athena
+        </a>
+        ,
+        <a href="https://github.com/joaoarthurv" target="_blank">
+          {" "}
+          João
+        </a>
+        ,
+        <a href="https://github.com/jenifferlaila" target="_blank">
+          {" "}
+          Jeniffer
+        </a>{" "}
+        e
+        <a href="https://github.com/leo-alvarenga" target="_blank">
+          {" "}
+          Leonardo
+        </a>{" "}
+      </p>
     </footer>
   );
 }
